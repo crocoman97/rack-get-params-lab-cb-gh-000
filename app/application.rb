b8be0a3dfe59e038c9e1b17590dd9c0eb5aa9c8a
Rauof M <abdelrauof197@gmail.com>
2018-02-14 06:14:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,49 +2,49 @@ class Application
 
   @@items = ["Apples","Carrots","Pears"]
   @@cart = []
-
+  
   def call(env)
-    resp = Rack::Response.new
+    
+    resp = Rack::Response.new 
     req = Rack::Request.new(env)
-
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    elsif req.path.match(/cart/)
+    
+    if req.path.match("/items/")
+      @@items.each { |i|
+        resp.write "#{i}\n"
+      }
+    elsif req.path.match("/cart/")
       if @@cart.empty?
         resp.write "Your cart is empty"
       else
-        @@cart.each do |item|
-          resp.write "#{item}\n"
-        end
+        @@cart.each { |i|
+          resp.write "#{i}\n"
+        }
       end
-    elsif req.path.match(/add/)
-      item_to_add = req.params["item"]
-      if @@items.include? item_to_add 
-        @@cart << item_to_add
-        resp.write "added #{item_to_add}"
-      else
-        resp.write "We don't have that item!"
+      
+    elsif req.path.match("/add/")
+      add_item = req.params["item"]
+      if @@items.include?(add_item)
+        @@cart << add_item
+        resp.write "#{add_item} is one of our cart"
+      else 
+        resp.write "Couldn't find #{add_item}"
+      end
+      
+    elsif req.path.match("/search/")
+      search_term = req.params["q"]
+      if @@items.include?(search_term)
+        resp.write "#{search_term} is one of our items"
+      else 
+        resp.write "Couldn't find #{search_term}"
       end
     else
       resp.write "Path Not Found"
     end
-
     resp.finish
   end
-
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
+    
 end
+
 
 
 
